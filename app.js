41e67015d9dcd0f17e99c4ace3b26d515dd8d38c
Ricardo Nava <RichyNavaMtz@hotmail.com>
2022-07-28 02:30:38 -0500

    setting EJS on the proyect

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ const express = require('express');
 //falta poner el directorio estatico __diname para que se pueda deployar
 const app = express();
 
+//seteamos app para usar EJS
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
+
+    const today = new Date();
+    const currentDay = today.getDay();
+    let day = '';
+    if(currentDay == 6 | currentDay == 0){
+        //cuando es fin de semana
+        day = 'weeknd!';
+        res.render('list',{kindOfDay:day});
+    }else{
+        //cuando es dia laboral
+        day = 'weekday!';
+        res.render('list',{kindOfDay:day})
+    }
+        
+    
+
+
+
+    
 })
 
 

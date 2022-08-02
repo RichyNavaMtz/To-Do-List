e9ebcdf179d977b55f2d973ae0f203a32f02677c
Ricardo Nava <RichyNavaMtz@hotmail.com>
2022-08-02 03:08:01 -0500

    implementando base de datos mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
 
-//falta poner el directorio estatico __diname para que se pueda deployar
+const express = require('express');
 const app = express();
+
+//require la base de datos
+const mongoose = require('mongoose');
 
 //seteamos app para usar EJS
 app.set('view engine','ejs');
@@ -12,29 +14,57 @@ app.use(express.urlencoded({extended:true}));
 //para servir el archivo de estilos
 app.use(express.static('public'));
 
-var items = [];
+//----------DB------------------------
+mongoose.connect('mongodb://localhost:27017/todolistDB')
+
+// items schema
+const itemsSchema = {
+    name: String
+};
+
+//mongoose model based on the schema
+const Item = mongoose.model('item', itemsSchema);
 
 app.get('/',(req,res)=>{
-    const today = new Date();
-    const options ={
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-    const day = today.toLocaleDateString('en-US',options);
-    res.render('list',{kindOfDay:day,newListItems:items});
-})
+    Item.find({},(err,listItems)=>{
+        res.render('list',{listName:'Today',listItems:listItems});
+    })
+});
 
 app.post('/',(req,res)=>{
-    let nuevo = req.body.newItem;
-    items.push(nuevo);
+    let newItem = req.body.newItem;
+    const item = new Item({name:newItem});
+    item.save();
     res.redirect('/')
 })
 
+app.post('/delete',(req,res)=>{
+    let doneId = req.body.done;
+    doneId = String(doneId)
+    Item.findByIdAndRemove(doneId,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log('item borrado');
+        }
+    })
+    res.redirect('/')
+})
 
+const listSchema = {
+    name: String,
+    items:[itemsSchema]
+}
+const List = mongoose.model('List',listSchema)
 
-
-
-
+// app.get('/:listName',(req,res)=>{
+//     let listName = req.params.listName
+//     const list = new List({
+//         name:listName,
+//         items:[]
+//     })
+//     list.save()
+//     res.render('list',{listName:listName,listItems:list})
+// })
 
 app.listen(3000,console.log('server running on port 3000'));

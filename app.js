30d7ee537933b594785db83efe127d2d6c7e321f
Ricardo Nava <RichyNavaMtz@hotmail.com>
2022-08-05 03:24:04 -0500

    routing dinamico integrado, falta delete del routing dinamico

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 //require la base de datos
 const mongoose = require('mongoose');
 
+//para el routing dinamico
+const _ = require('lodash');
+
 //seteamos app para usar EJS
 app.set('view engine','ejs');
 
@@ -25,23 +28,38 @@ const itemsSchema = {
 //mongoose model based on the schema
 const Item = mongoose.model('Item', itemsSchema);
 
+const item1 = new Item({
+    name: 'Welcome to your todolist!'
+});
+const item2 = new Item({
+    name: 'Hit the + button to add a new item'
+})
+const item3 = new Item({
+    name:'Hit the trash can to delete an item'
+})
+
+const defaultItems = [item1, item2, item3];
+
+// list 
+const listSchema = {
+    name:String,
+    items:[itemsSchema]
+};
+
+const List = mongoose.model('List', listSchema);
 
 
 
 app.get('/',(req,res)=>{
-    Item.find({},(err,listItems)=>{
-        res.render('list',{listTitle:'Today',listItems:listItems});
+    Item.find({},(err,foundItems)=>{
+        
+        res.render('list',{listTitle:'Today',newListItems:foundItems})
+        
     })
 });
 
-app.post('/',(req,res)=>{
-    let newItem = req.body.newItem;
-    const item = new Item({name:newItem});
-    item.save();
-    res.redirect('/')
-})
-
 app.post('/delete',(req,res)=>{
+    let listName = req.body.list
     let doneId = req.body.done;
     doneId = String(doneId)
     Item.findByIdAndRemove(doneId,(err)=>{
@@ -53,6 +71,48 @@ app.post('/delete',(req,res)=>{
     })
     res.redirect('/')
 })
+
+app.get('/:customListName',(req,res)=>{
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({name: customListName}, function(err, foundList){
+        if (!err){
+          if (!foundList){
+            //Create a new list
+            const list = new List({
+              name: customListName,
+              items: []
+            });
+            list.save();
+            res.redirect("/" + customListName);
+          } else {
+                //Show an existing list
+    
+                res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+            }
+    }
+});
+});
+
+
+app.post('/',(req,res)=>{
+    let newItem = req.body.newItem;
+    let listName = req.body.list
+    const item = new Item({name:newItem});
+    if(listName === 'Today'){
+        item.save();
+        res.redirect('/')
+    }else{
+        List.findOne({name:listName},(err,foundList)=>{
+            foundList.items.push(item)
+            foundList.save()
+            res.redirect('/'+listName)
+        })
+    }
+    
+})
+
+
+
 
 
 
